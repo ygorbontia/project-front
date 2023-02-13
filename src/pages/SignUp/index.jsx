3affbbdd3f15be15fec9e7aeec0f468fab22cb0d
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-13 19:43:47 -0300

    update SignUp page structure

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,35 @@
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { FiUser, FiMail, FiLock } from 'react-icons/fi';
+
+import { SignUpSC, FormSC, BackgroundSC } from './style';
+
+import { UserInput } from '../../componentes/UserInput';
+import { Button } from '../../componentes/Button';
+
 export function SignUp() {
   return (
-    <h1>SignUp</h1>
+    <SignUpSC>
+      <FormSC>
+        <div>
+          <h1>RocketMovies</h1>
+          <p>Aplicação para acompanhar tudo que assistir.</p>
+
+          <h2>Crie sua conta</h2>
+        </div>
+
+        <form>
+          <UserInput icon={ FiUser } type="text" placeholder="Nome" />
+          <UserInput icon={ FiMail } type="email" placeholder="E-mail" />
+          <UserInput icon={ FiLock } type="password" placeholder="Senha" />
+        </form>
+
+        <a href="#">
+          <AiOutlineArrowLeft />
+          Voltar para o login
+        </a>
+      </FormSC>
+
+      <BackgroundSC />
+    </SignUpSC>
   )
 }
--- src/pages/SignUp/style.js
+++ src/pages/SignUp/style.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
 
-export const SignUpSC = styled.div`
+import backgroundImage from '../../assets/background.jpg';
 
+export const SignUpSC = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: row;
+`;
+
+export const FormSC = styled.div`
+
+`;
+
+export const BackgroundSC = styled.div`
+  flex: 1;
+
+  background-image: url( ${ backgroundImage } );
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  filter: brightness(.3);
 `;
