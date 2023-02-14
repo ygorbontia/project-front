f83c7b6c2a0a03285757f71cf5258e1e9df3b61b
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-14 17:57:07 -0300

    update Header component style

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
 export const InputSC = styled.input`
-  
+  width: 100%;
+  height: 5.6rem;
+  background-color: ${({ theme }) => theme.dark_300};
+  border: none;
+  border-radius: 10px;
+  padding: 1.9rem 2.4rem;
+
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.light_100};
+
+  &::placeholder {
+    font-size: 1.4rem;
+    color: ${({ theme }) => theme.light_500};
+  }
 `;
