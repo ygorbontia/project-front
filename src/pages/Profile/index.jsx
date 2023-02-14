e1946c9fda4c61729b68dcd3d980c858103c5476
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-14 14:53:00 -0300

    update Profile page structure

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,35 @@
+import { FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { BiCamera } from 'react-icons/bi';
+import { ProfileSC, UserAvatarSC } from './style';
+
+import { TextButton } from '../../components/TextButton';
+import { UserInput } from '../../components/UserInput';
+import { Button } from '../../components/Button';
+
 export function Profile() {
   return (
-    <h1>Profile</h1>
+    <ProfileSC>
+      <header>
+        <TextButton title="Voltar" />
+      </header>
+
+      <form>
+        <UserAvatarSC>
+          <img src="https://github.com/ygorbontia.png" alt="" />
+
+          <span>
+            <BiCamera size={ 20 } />
+            <input type="file" />
+          </span>
+        </UserAvatarSC>
+
+        <UserInput icon={ FiUser } type="text" placeholder="Nome" />
+        <UserInput icon={ FiMail } type="email" placeholder="E-mail" />
+        <UserInput icon={ FiLock } type="password" placeholder="Senha atual" />
+        <UserInput icon={ FiLock } type="password" placeholder="Nova senha" />
+
+        <Button title="Salvar" />
+      </form>
+    </ProfileSC>
   )
 }
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -3,3 +3,7 @@ import styled from 'styled-components';
 export const ProfileSC = styled.div`
 
 `;
+
+export const UserAvatarSC = styled.div`
+
+`;
