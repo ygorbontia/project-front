ff38fdd7e89c615bdd9827942d22bb158b67c787
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-14 20:07:14 -0300

    update TextButton component style

--- src/components/TextButton/style.js
+++ src/components/TextButton/style.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
 export const TextButtonSC = styled.button`
+  background-color: transparent;
+  border: none;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: .8rem;
+
+  cursor: pointer;
+
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.pink_rgb};
 `;
