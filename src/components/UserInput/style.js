7c13facff91f8f3f341e24ff6db3f12896f40e23
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-14 20:14:14 -0300

    update UserInput component style

--- src/components/UserInput/style.js
+++ src/components/UserInput/style.js
@@ -2,6 +2,30 @@ import styled from 'styled-components';
 
 export const UserInputSC = styled.div`
   width: 100%;
+  height: 5.6rem;
+  background-color: ${({ theme }) => theme.dark_300};
+  border: none;
+  border-radius: 10px;
+  margin-bottom: .8rem;
+  padding: 0 1.2rem;
 
   display: flex;
+  align-items: center;
+  gap: 1.2rem;
+
+  > svg {
+    color: ${({ theme }) => theme.light_500};
+  }
+
+  > input {
+    background-color: transparent;
+    border: none;
+
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.light_100};
+
+    &::placeholder {
+      color: ${({ theme }) => theme.light_500};
+    }
+  }
 `;
