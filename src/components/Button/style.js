354ffea6630b9cbd76b9ae50928b215b8545184d
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-14 18:02:26 -0300

    update Button component style

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -2,4 +2,11 @@ import styled from 'styled-components';
 
 export const ButtonSC = styled.button`
   width: 100%;
+  height: 5.6rem;
+  background-color: ${({ theme }) => theme.pink_rgb};
+  border-radius: 10px;
+
+  font-weight: 500;
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.dark_200};
 `;
