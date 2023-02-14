63bdda0d8420fa863861a15ff24d30eb18e7dc8f
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-14 20:05:11 -0300

    update profile page style

--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -1,7 +1,27 @@
 import styled from 'styled-components';
 
 export const ProfileSC = styled.div`
+  width: 100%;
+  height: 100vh;
+  
+  display: grid;
+  grid-template-rows: 14.4rem auto;
+  grid-template-areas:
+    "header"
+    "content";
 
+  > header {
+    grid-area: "header";
+
+    width: 100%;
+    height: 14.4rem;
+    background-color: ${({ theme }) => theme.pink_rgba};
+    border: none;
+    padding: 0 14.4rem;
+
+    display: flex;
+    align-items: center;
+  }
 `;
 
 export const UserAvatarSC = styled.div`
