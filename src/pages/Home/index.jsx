9a951cc3fbdb8d535d4cf18a5b24f78c9d0c5d19
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-14 19:18:03 -0300

    complete Home page style and update page structure

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,49 +11,47 @@ export function Home() {
     <HomeSC>
       <Header />
 
-      <main>
-        <div className="movies">
-          <h1>Meus filmes</h1>
+      <div className="movies">
+        <h1>Meus filmes</h1>
 
-          <button>
-            <AiOutlinePlus size={ 20 } />
-            Adicionar filme
-          </button>
-        </div>
+        <button>
+          <AiOutlinePlus size={ 20 } />
+          Adicionar filme
+        </button>
+      </div>
 
-        <MovieNotesSC>
-          <Notes 
-            title="Interstellar"
-            rating="5"
-            description='Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de'
-            tags={[ 
-              { id: 1, name: "Ficção Científica"},
-              { id: 2, name: "Drama"},
-              { id: 3, name: "Família"}
-            ]}
-          />
-          <Notes 
-            title="Interstellar"
-            rating="5"
-            description='Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de'
-            tags={[ 
-              { id: 1, name: "Ficção Científica"},
-              { id: 2, name: "Drama"},
-              { id: 3, name: "Família"}
-            ]}
-          />
-          <Notes 
-            title="Interstellar"
-            rating="5"
-            description='Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de'
-            tags={[ 
-              { id: 1, name: "Ficção Científica"},
-              { id: 2, name: "Drama"},
-              { id: 3, name: "Família"}
-            ]}
-          />
-        </MovieNotesSC>
-      </main>
+      <MovieNotesSC>
+        <Notes 
+          title="Interstellar"
+          rating="5"
+          description='Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de'
+          tags={[ 
+            { id: 1, name: "Ficção Científica"},
+            { id: 2, name: "Drama"},
+            { id: 3, name: "Família"}
+          ]}
+        />
+        <Notes 
+          title="Interstellar"
+          rating="5"
+          description='Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de'
+          tags={[ 
+            { id: 1, name: "Ficção Científica"},
+            { id: 2, name: "Drama"},
+            { id: 3, name: "Família"}
+          ]}
+        />
+        <Notes 
+          title="Interstellar"
+          rating="5"
+          description='Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de'
+          tags={[ 
+            { id: 1, name: "Ficção Científica"},
+            { id: 2, name: "Drama"},
+            { id: 3, name: "Família"}
+          ]}
+        />
+      </MovieNotesSC>
     </HomeSC>
   )
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,9 +1,72 @@
 import styled from 'styled-components';
 
 export const HomeSC = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding-bottom: 5.8rem;
 
+  display: grid;
+  grid-template-rows: 11.6rem auto;
+  grid-template-areas: 
+    "header"
+    "content";
+  
+  > .movies {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 113.7rem;
+    margin: 4.7rem auto 3.7rem;
+
+    h1 {
+      font-weight: 400;
+      font-size: 3.2rem;
+      color: ${({ theme }) => theme.white};
+    }
+
+    button {
+      height: 4.8rem;
+      background-color: ${({ theme }) => theme.pink_rgb};
+      border: none;
+      border-radius: 8px;
+      padding: 1.35rem 3.2rem;
+      
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: .8rem;
+
+      cursor: pointer;
+      
+      font-weight: 400;
+      font-size: 1.6rem;
+      color: ${({ theme }) => theme.dark_500};
+    }
+  }
 `;
 
 export const MovieNotesSC = styled.div`
+  grid-area: "content";
 
+  width: 113.7rem;
+  margin: 0 auto;
+  overflow-y: auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.pink_rgb};
+    border-radius: 8px;
+  }
 `;
