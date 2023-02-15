3c87aba48a66c44e9d25b2a33a372fc9b1b33b9f
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-15 14:51:04 -0300

    update Details page style

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,5 @@
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { CiClock2 } from 'react-icons/ci';
 
 import { DetailsSC } from './style';
 
@@ -11,21 +12,41 @@ export function Details() {
     <DetailsSC>
       <Header />
 
-      <main>
+      <nav>
         <TextButton title="Voltar" />
+      </nav>
 
+      <main>
         <section>
-          <div>
-            <h1>Interestelar</h1>
+          <div className="movie-and-author">
+            <div className="movie">
+              <h1>Interestelar</h1>
 
-            <AiFillStar size={ 20 } />
-            <AiFillStar size={ 20 } />
-            <AiFillStar size={ 20 } />
-            <AiFillStar size={ 20 } />
-            <AiOutlineStar size={ 20 } />
+              <div className="rating">
+                <AiFillStar size={ 20 } />
+                <AiFillStar size={ 20 } />
+                <AiFillStar size={ 20 } />
+                <AiFillStar size={ 20 } />
+                <AiOutlineStar size={ 20 } />
+              </div>
+            </div>
+
+            <div className="author-and-time">
+              <div className="author">
+                <img src="https://github.com/ygorbontia.png" alt="" />
+
+                <p>Por Ygor Bontia</p>
+              </div>
+
+              <div className="time">
+                <CiClock2 size={ 16 } />
+
+                <p>15/02/23 às 14:36</p>
+              </div>
+            </div>
           </div>
 
-          <div>
+          <div className="tags">
             <DetailsTag
               title="Ficção Científica"
             />
@@ -37,11 +58,15 @@ export function Details() {
             />
           </div>
 
-          <p>
-            Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy.
-            <br />
-            Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly, o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller, porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua: cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem que 23 anos se passaram.
-          </p>
+          <div className="description">
+            <p>
+              Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy.
+            </p>
+
+            <p>
+              Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly, o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller, porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua: cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem que 23 anos se passaram.
+            </p>
+          </div>
         </section>
       </main>
     </DetailsSC>
--- src/pages/Details/style.js
+++ src/pages/Details/style.js
@@ -3,22 +3,40 @@ import styled from 'styled-components';
 export const DetailsSC = styled.div`
   width: 100%;
   height: 100vh;
+  padding-bottom: 15.6rem;
 
   display: grid;
   grid-template-rows: 11.6rem auto;
   grid-template-areas:
     "header"
+    "nav"
     "content";
+
+  > nav {
+    grid-area: "nav";
+
+    width: 113.7rem;
+    margin: 4rem auto 2.4rem;
+  }
 
   > main {
     grid-area: "content";
+    overflow-y: auto;
     
     width: 113.7rem;
-    margin: 4rem auto 0;
-    padding-bottom: 15.6rem;
-    
-    > button {
-      margin-bottom: 2.4rem;
+    margin: 0 auto;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: ${({ theme }) => theme.pink_rgb};
+      border-radius: 8px;
     }
     
     > section {
@@ -28,6 +46,85 @@ export const DetailsSC = styled.div`
 
       display: grid;
       gap: 4rem;
+
+      .movie-and-author {
+        display: flex;
+        flex-direction: column;
+        gap: 2.4rem;
+
+        .movie {
+          display: flex;
+          align-items: center;
+          gap: 2rem;
+
+          h1 {
+            font-weight: 500;
+            font-size: 3.6rem;
+            color: ${({ theme }) => theme.light_100};
+          }
+
+          .rating {
+            display: flex;
+            gap: 1rem;
+
+            height: 2rem;
+            color: ${({ theme }) => theme.pink_rgb};
+          }
+        }
+
+        .author-and-time {
+          display: flex;
+          align-items: center;
+          gap: 1.5rem;
+  
+          p {
+            font-size: 1.6rem;
+            color: ${({ theme }) => theme.light_100};
+          }
+
+          .author {
+            display: flex;
+            align-items: center;
+            gap: .8rem;
+
+            img {
+              width: 1.6rem;
+              height: 1.6rem;
+              border: solid 1px ${({ theme }) => theme.dark_100};
+              border-radius: 50%;
+            }
+          }
+
+          .time {
+            display: flex;
+            align-items: center;
+            gap: .8rem;
+
+            svg {
+              color: ${({ theme }) => theme.pink_rgb};
+            }
+          }
+        }
+      }
+
+      .tags {
+        display: flex;
+        align-items: center;
+        gap: .8rem;
+      }
+
+      .description {
+        p {
+          font-weight: 400;
+          font-size: 1.6rem;
+          color: ${({ theme }) => theme.light_100};
+          text-align: justify;
+        }
+
+        p + p {
+          margin-top: 2.2rem;
+        }
+      }
     }
   }
 `;
