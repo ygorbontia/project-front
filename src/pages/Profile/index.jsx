cb5827b0b7dd31add1904093910ab6edd0dce3a6
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-15 10:36:29 -0300

    update Profile page structure and complete page style

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,4 @@
-import { FiUser, FiMail, FiLock } from 'react-icons/fi'
-import { BiCamera } from 'react-icons/bi';
+import { FiCamera, FiUser, FiMail, FiLock } from 'react-icons/fi'
 import { ProfileSC, UserAvatarSC } from './style';
 
 import { TextButton } from '../../components/TextButton';
@@ -13,23 +12,30 @@ export function Profile() {
         <TextButton title="Voltar" />
       </header>
 
-      <form>
-        <UserAvatarSC>
-          <img src="https://github.com/ygorbontia.png" alt="" />
+      <main>
+        <form>
+          <UserAvatarSC>
+            <img src="https://github.com/ygorbontia.png" alt="" />
 
-          <span>
-            <BiCamera size={ 20 } />
-            <input type="file" />
-          </span>
-        </UserAvatarSC>
+            <label>
+              <FiCamera size={ 20 } />
+              <input type="file" />
+            </label>
+          </UserAvatarSC>
 
-        <UserInput icon={ FiUser } type="text" placeholder="Nome" />
-        <UserInput icon={ FiMail } type="email" placeholder="E-mail" />
-        <UserInput icon={ FiLock } type="password" placeholder="Senha atual" />
-        <UserInput icon={ FiLock } type="password" placeholder="Nova senha" />
+          <UserInput icon={ FiUser } type="text" placeholder="Nome" />
+          <UserInput icon={ FiMail } type="email" placeholder="E-mail" />
+          <UserInput icon={ FiLock } type="password" placeholder="Senha atual" />
+          <UserInput icon={ FiLock } type="password" placeholder="Nova senha" />
 
-        <Button title="Salvar" />
-      </form>
+          <Button title="Salvar" />
+        </form>
+        
+        <button>
+          Excluir conta
+        </button>
+      </main>
+
     </ProfileSC>
   )
 }
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -10,6 +10,8 @@ export const ProfileSC = styled.div`
     "header"
     "content";
 
+  text-align: center;
+
   > header {
     grid-area: "header";
 
@@ -22,8 +24,73 @@ export const ProfileSC = styled.div`
     display: flex;
     align-items: center;
   }
+
+  > main {
+    width: 34rem;
+    margin: 0 auto 6.4rem;
+
+    > form {
+      margin-bottom: 6.4rem;
+
+      > div:nth-child(3) {
+        margin-bottom: 2.4rem;
+      }
+      > div:nth-child(5) {
+        margin-bottom: 2.4rem;
+      }
+    }
+    
+    > button {
+      background-color: transparent;
+      border: none;
+  
+      cursor: pointer;
+  
+      font-size: 1.6rem;
+      color: ${({ theme }) => theme.pink_rgb};
+    }
+  }
+
 `;
 
 export const UserAvatarSC = styled.div`
+  width: 18.6rem;
+  height: 18.6rem;
+  margin: -9.3rem auto 6.4rem;
 
+  position: relative;
+
+  > img {
+    width: 18.6rem;
+    height: 18.6rem;
+    border: none;
+    border-radius: 50%;
+  }
+
+  > label {
+    width: 4.8rem;
+    height: 4.8rem;
+    background-color: ${({ theme }) => theme.pink_rgb};
+    border: none;
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    right: 1rem;
+    bottom: .4rem;
+
+
+    cursor: pointer;
+
+    > svg {
+      color: ${({ theme }) => theme.dark_200};
+    }
+
+    input {
+      display: none;
+    }
+  }
 `;
