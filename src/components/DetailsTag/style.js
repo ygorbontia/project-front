e371a2790f211d04b7ad6ac82f21c7bfa540b314
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-15 14:16:45 -0300

    update DetailsTag component style

--- src/components/DetailsTag/style.js
+++ src/components/DetailsTag/style.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
 export const DetailsTagSC = styled.span`
+  background-color: ${({ theme }) => theme.dark_400};
+  border: none;
+  border-radius: 8px;
+  padding: .8rem 1.6rem;
 
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.light_200};
 `;
--- src/pages/Details/style.js
+++ src/pages/Details/style.js
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
 
 export const DetailsSC = styled.div`
+  width: 100%;
+  height: 100vh;
 
+  display: grid;
+  grid-template-rows: 11.6rem auto;
+  grid-template-areas:
+    "header"
+    "content";
+
+  > main {
+    grid-area: "content";
+    
+    width: 113.7rem;
+    margin: 4rem auto 0;
+    padding-bottom: 15.6rem;
+    
+    > button {
+      margin-bottom: 2.4rem;
+    }
+    
+    > section {
+      width: 100%;
+
+      overflow-y: auto;
+
+      display: grid;
+      gap: 4rem;
+    }
+  }
 `;
