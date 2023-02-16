31abf0f89505d0bd2921db4644d5fc51898e6ab0
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-16 16:06:04 -0300

    update routes to get user data for authentication

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,16 @@
+import { useAuth } from '../hooks/auth';
+
 import { BrowserRouter } from 'react-router-dom';
 
 import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      { user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
