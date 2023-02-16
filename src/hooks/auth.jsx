670765af5832357e5d7668a9f5ae444b8aa53089
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-16 14:39:44 -0300

    create function to signIn

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,8 +1,23 @@
 import { createContext, useContext } from 'react';
 
+import { api } from '../services/api';
+
 const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível efetuar o login")
+      }
+    }
+
+  }
+
   return (
     <AuthContext.Provider value={{}}>
       { children }
