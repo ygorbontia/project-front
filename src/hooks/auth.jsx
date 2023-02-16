562aa1874d48df0a1264cb7fceee77e1eadf5738
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-16 16:44:36 -0300

    update authentication context, create a signOut function

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 import { api } from '../services/api';
 
@@ -12,6 +12,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
+      localStorage.setItem("@rocketmovies:user", JSON.stringify(user));
+      localStorage.setItem("@rocketmovies:token", token);
+
       api.defaults.headers.common['Authorization'] = `Bearer ${ token }`;
       setData({ user, token });
 
@@ -22,11 +25,31 @@ function AuthProvider({ children }) {
         alert("Não foi possível efetuar o login")
       }
     }
-
   }
 
+  function signOut() {
+    localStorage.removeItem("@rocketmovies:user");
+    localStorage.removeItem("@rocketmovies:token");
+
+    setData({})
+  }
+
+  useEffect(() => {
+    const user = localStorage.getItem("@rocketmovies:user");
+    const token = localStorage.getItem("@rocketmovies:token");
+
+    if (user && token) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${ token }`;
+
+      setData({ 
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider value={{ signIn, user: data.user, signOut }}>
       { children }
     </AuthContext.Provider>
   )
